15266bdc0ac2904c166b076d93cad934989d20a9
Raman Yadav <yramraman@gmail.com>
2026-01-15 20:10:06 +0545

    Commit By Weekly Assignment week_1_2 part 1 problem_09

---
+++ Logbook-FOCP/By_weekly_assignment/Assignment_1-week1_2/Part_01/Problem_09.py
@@ -0,0 +1,15 @@
+'''
+9.The integer part of 9.9999 and the rounded value to 2 decimal places.
+'''
+# Given number
+number = 9.9999
+
+# Integer part
+integer_part = int(number)
+
+# Rounded value to 2 decimal places
+rounded_value = round(number, 2)
+
+# Display the results
+print("Integer part:", integer_part)
+print("Rounded value (2 decimal places):", rounded_value)
